ab70033f82b7b4d2b5c7529c77f84301b8c5cec5
AndreiPanait <andrei.panait@3amail.it>
2022-05-05 14:21:58 +0200

    Initial App

---
+++ app/src/main/java/com/andreispanait/netappnotification/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andreispanait.netappnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
